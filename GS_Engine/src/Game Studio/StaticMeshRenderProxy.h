aad77f007d7352f32255f0c5fe458d42bc17d9d7
Facundo961 <facundovilla961@gmail.com>
2019-02-04 21:47:18 -0300

    Added RenderProxy class.

---
+++ GS_Engine/src/Game Studio/StaticMeshRenderProxy.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Core.h"
+
+#include "RenderProxy.h"
+
+GS_CLASS StaticMeshRenderProxy : public RenderProxy
+{
+public:
+	StaticMeshRenderProxy();
+	~StaticMeshRenderProxy();
+};
+

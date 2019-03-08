6d679bb3b11c9bafd1a4a76795b78f57c61b4dae
Facundo961 <facundovilla961@gmail.com>
2019-03-08 02:31:29 -0300

    Updated StaticMeshResource implementation.

--- GS_Engine/src/Game Studio/StaticMeshResource.cpp
+++ GS_Engine/src/Game Studio/StaticMeshResource.cpp
@@ -63,59 +63,73 @@ Mesh * StaticMeshResource::ProcessNode(aiNode * Node, const aiScene * Scene)
 
 Mesh StaticMeshResource::ProcessMesh(aiMesh * InMesh)
 {
-	Mesh LocMesh;
+	//Create a mesh object to hold the mesh currently being processed.
+	Mesh Result;
 
-	LocMesh.VertexArray = new Vertex[InMesh->mNumVertices];
+	//--MESH SETUP--
+
+	//We allocate a new array of vertices big enough to hold the number of vertices in this mesh and assign it to the
+	//pointer found inside the mesh.
+	Result.VertexArray = new Vertex[InMesh->mNumVertices];
+
+	//Set this mesh's vertex count as the number of vertices found in this mesh.
+	Result.VertexCount = InMesh->mNumVertices;
+
+	//--MESH SETUP--
 
 	// Loop through each vertex.
-	for (unsigned int i = 0; i < InMesh->mNumVertices; i++)
+	for (auto i = 0; i < InMesh->mNumVertices; i++)
 	{
 		// Positions
-		LocMesh.VertexArray[i].Position.X = InMesh->mVertices[i].x;
-		LocMesh.VertexArray[i].Position.Y = InMesh->mVertices[i].y;
-		LocMesh.VertexArray[i].Position.Z = InMesh->mVertices[i].z;
+		Result.VertexArray[i].Position.X = InMesh->mVertices[i].x;
+		Result.VertexArray[i].Position.Y = InMesh->mVertices[i].y;
+		Result.VertexArray[i].Position.Z = InMesh->mVertices[i].z;
 
 		// Normals
-		LocMesh.VertexArray[i].Normal.X = InMesh->mNormals[i].x;
-		LocMesh.VertexArray[i].Normal.Y = InMesh->mNormals[i].y;
-		LocMesh.VertexArray[i].Normal.Z = InMesh->mNormals[i].z;
+		Result.VertexArray[i].Normal.X = InMesh->mNormals[i].x;
+		Result.VertexArray[i].Normal.Y = InMesh->mNormals[i].y;
+		Result.VertexArray[i].Normal.Z = InMesh->mNormals[i].z;
 
-		// texture coordinates
-		if (InMesh->mTextureCoords[0]) // does the mesh contain texture coordinates?
+		// Texture Coordinates
+		if (InMesh->mTextureCoords[0]) //We check if the pointer to texture coords is valid. (Could be NULLPTR)
 		{
-			// a vertex can contain up to 8 different texture coordinates. We thus make the assumption that we won't 
-			// use models where a vertex can have multiple texture coordinates so we always take the first set (0).
-			LocMesh.VertexArray[i].TextCoord.U = InMesh->mTextureCoords[0][i].x;
-			LocMesh.VertexArray[i].TextCoord.V = InMesh->mTextureCoords[0][i].y;
+			//A vertex can contain up to 8 different texture coordinates.
+			//Here, we are making the asumption we won't be using a model with more than one texture coordinates.
+			Result.VertexArray[i].TextCoord.U = InMesh->mTextureCoords[0][i].x;
+			Result.VertexArray[i].TextCoord.V = InMesh->mTextureCoords[0][i].y;
 		}
-		/*
+
 		// Tangent
-		LocMesh.VertexArray[i].Tangent.X = InMesh->mTangents[i].x;
-		LocMesh.VertexArray[i].Tangent.Y = InMesh->mTangents[i].y;
-		LocMesh.VertexArray[i].Tangent.Z = InMesh->mTangents[i].z;
+		Result.VertexArray[i].Tangent.X = InMesh->mTangents[i].x;
+		Result.VertexArray[i].Tangent.Y = InMesh->mTangents[i].y;
+		Result.VertexArray[i].Tangent.Z = InMesh->mTangents[i].z;
 
 		// BiTangent
-		LocMesh.VertexArray[i].BiTangent.X = InMesh->mBitangents[i].x;
-		LocMesh.VertexArray[i].BiTangent.Y = InMesh->mBitangents[i].y;
-		LocMesh.VertexArray[i].BiTangent.Z = InMesh->mBitangents[i].z;
-		*/
+		Result.VertexArray[i].BiTangent.X = InMesh->mBitangents[i].x;
+		Result.VertexArray[i].BiTangent.Y = InMesh->mBitangents[i].y;
+		Result.VertexArray[i].BiTangent.Z = InMesh->mBitangents[i].z;
 	}
 
-	LocMesh.IndexArray = new unsigned int[InMesh->mNumFaces];
+	//We allocate a new array of unsigned ints big enough to hold the number of indices in this mesh and assign it to the
+	//pointer found inside the mesh.
+	Result.IndexArray = new unsigned int[InMesh->mNumFaces];
 
-	// now wak through each of the mesh's faces (a face is a mesh its triangle) and retrieve the corresponding vertex indices.
-	for (unsigned int f = 0; f < InMesh->mNumFaces; f++)
+	//Wow loop through each of the mesh's faces and retrieve the corresponding vertex indices.
+	for (auto f = 0; f < InMesh->mNumFaces; f++)
 	{
-		aiFace Face = InMesh->mFaces[f];
+		const aiFace Face = InMesh->mFaces[f];
 
-		// retrieve all indices of the face and store them in the indices vector
-		for (unsigned int j = 0; j < Face.mNumIndices; j++)
+		//Update the vertex count by summing the number of indices that each face we loop through has.
+		Result.VertexCount += Face.mNumIndices;
+
+		// Retrieve all indices of the face and store them in the indices array.
+		for (auto j = 0; j < Face.mNumIndices; j++)
 		{
-			LocMesh.IndexArray[j] = Face.mIndices[j];
+			Result.IndexArray[j] = Face.mIndices[j];
 		}
 	}
 
-	return LocMesh;
+	return Result;
 }
 
 Mesh * StaticMeshResource::LoadFallbackResource() const

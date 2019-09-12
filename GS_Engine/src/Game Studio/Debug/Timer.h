e627f0071b15633a39430996836d46b6c1b66d1a
Facundo Villa <facundovilla961@gmail.com>
2019-09-12 13:17:58 -0300

    Create Timer.h

---
+++ GS_Engine/src/Game Studio/Debug/Timer.h
@@ -0,0 +1,19 @@
+#include "../Clock.h"
+
+#include "FString.h"
+
+class Timer
+{
+    Nanoseconds StartingTime = 0;
+    FString Name = "No name!";
+public:
+    Timer(const FString& _Name);
+    ~Timer();
+};
+
+#ifdef GS_DEBUG
+//Places a timer which automatically starts counting. Timer will stop and print results when it exits the scope it was created in.
+#define PLACE_TIMER(name) Timer LocalTimer(FString(name));
+#else
+#define PLACE_TIMER(name)
+#endif

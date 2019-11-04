2017cd1ee97af61f7508c76a83d5a7df5f6a051f
Facundo961 <facundovilla961@gmail.com>
2019-11-04 01:17:32 -0300

    Added concept audio classes.

---
+++ GS_Engine/src/Game Studio/SAPI/AudioCore.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include "Core.h"
+
+/**
+ * \brief Names all possible audio channel counts.
+ */
+enum class AudioChannelCount : uint8
+{
+	/**
+	 * \brief Audio is mono, has only one channel.
+	 */
+	CHANNELS_MONO,
+	
+	/**
+	 * \brief Audio is stereo, has two channels. Typical for speakers or headphones.
+	 */
+	CHANNELS_STEREO,
+	
+	/**
+	 * \brief Audio is in 5.1, has five channels and one sub-woofer channel. Typical for home cinema setups.
+	 */
+	CHANNELS_5_1,
+	
+	/**
+	 * \brief Audio is in 7.1, has seven channels and one sub-woofer channel. Typical for home cinema setups.
+	 */
+	CHANNELS_7_1
+};
+
+/**
+ * \brief Names all possible audio bit depths.
+ */
+enum class AudioBitDepth : uint8
+{
+	/**
+	 * \brief Audio bit depth is 8 bit.
+	 */
+	BIT_DEPTH_8,
+	
+	/**
+	 * \brief Audio bit depth is 16 bit.
+	 */
+	BIT_DEPTH_16,
+	
+	/**
+	 * \brief Audio bit depth is 24 bit.
+	 */
+	BIT_DEPTH_24
+};
+
+/**
+ * \brief Names all possible audio sample rates (kHz).
+ */
+enum class AudioSampleRate : uint8
+{
+	/**
+	 * \brief Audio sample rate is 44.100 Hz.
+	 */
+	KHZ_44_1,
+	
+	/**
+	* \brief Audio sample rate is 48.000 Hz.
+	 */
+	KHZ_48,
+
+	/**
+	* \brief Audio sample rate is 96.000 Hz.
+	*/
+	KHZ_96
+};
+
+/**
+ * \brief Names all possible audio output device types.
+ */
+enum class AudioOutputDeviceType : uint8
+{
+	/**
+	 * \brief Audio output device are speakers.
+	 */
+	SPEAKERS,
+
+	/**
+	 * \brief Audio output device are headphones.
+	 */
+	 HEADPHONES
+};
---
+++ GS_Engine/src/Game Studio/Sound/SoundListener.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/**
+ * \brief Describes a sound listener, which represents a point in space from which to capture 3D sound in a game world.
+ */
+class SoundListener
+{
+	
+public:
+};

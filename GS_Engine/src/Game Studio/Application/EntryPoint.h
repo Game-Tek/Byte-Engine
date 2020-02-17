eba8ce1b41d94acf9cc96931b8ba66a04f02ec35
Facundo961 <facundovilla961@gmail.com>
2020-02-17 20:16:08 -0300

    Revert "Changed comments position in entrypoint."
    This reverts commit 72e276bf132c63a8d512cd46661c21f3df463c37.

--- GS_Engine/src/Game Studio/Application/EntryPoint.h
+++ GS_Engine/src/Game Studio/Application/EntryPoint.h
@@ -4,10 +4,10 @@ extern GS::Application* GS::CreateApplication(); //Is defined in another transla
 
 int main(int argc, char** argv)
 {
-	//When CreateApplication() is defined it must return a new object of it class, effectively letting us manage that instance from here.
 	auto Application = GS::CreateApplication();
-	//Call Run() on Application. There lies the actual application code, like the Engine SubSystems' initialization, the game loop, etc.
+	//When CreateApplication() is defined it must return a new object of it class, effectively letting us manage that instance from here.
 	const auto exit_code = Application->Run(argc, argv);
+	//Call Run() on Application. There lies the actual application code, like the Engine SubSystems' initialization, the game loop, etc.
 	delete Application; //When Run() is done we delete the instance.
 
 	return exit_code; //Return success and exit.

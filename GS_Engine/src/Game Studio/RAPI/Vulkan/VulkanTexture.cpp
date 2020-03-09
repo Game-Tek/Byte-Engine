a9641ee38a7b74ca0fd8dc521e9c44883721ca2b
Facundo Villa <FacundoVilla961@gmail.com>
2020-03-09 14:08:46 -0300

    Fixed errors in VulkanTexture.

--- GS_Engine/src/Game Studio/RAPI/Vulkan/VulkanTexture.cpp
+++ GS_Engine/src/Game Studio/RAPI/Vulkan/VulkanTexture.cpp
@@ -15,8 +15,8 @@ VulkanTexture::VulkanTexture(VulkanRenderDevice* vulkanRenderDevice, const Textu
 	// COPY STAGING BUFFER TO IMAGE
 	// TRANSITION LAYOUT FROM TRANSFER_DST TO { DESIRED USE }
 
-	VkBuffer staging_buffer = VK_NULL_HANDLE;
-	VkDeviceMemory staging_buffer_memory = VK_NULL_HANDLE;
+	VkBuffer staging_buffer = nullptr;
+	VkDeviceMemory staging_buffer_memory = nullptr;
 
 	DArray<VkFormat> formats = { FormatToVkFormat(textureCreateInfo.ImageFormat), VK_FORMAT_R8G8B8A8_UNORM };
 
@@ -39,7 +39,12 @@ VulkanTexture::VulkanTexture(VulkanRenderDevice* vulkanRenderDevice, const Textu
 		}
 	}
 
-	CreateBuffer(vulkanRenderDevice->GetVkDevice(), &staging_buffer, supportedTextureSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_SHARING_MODE_EXCLUSIVE);
+	VkBufferCreateInfo bufferInfo{ VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO };
+	bufferInfo.size = supportedTextureSize;
+	bufferInfo.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+	bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+	VK_CHECK(vkCreateBuffer(vulkanRenderDevice->GetVkDevice(), &bufferInfo, vulkanRenderDevice->GetVkAllocationCallbacks(), &staging_buffer));
 
 	{
 		VkMemoryRequirements memRequirements;
@@ -93,11 +98,10 @@ VulkanTexture::VulkanTexture(VulkanRenderDevice* vulkanRenderDevice, const Textu
 	vk_image_create_info.arrayLayers = 1;
 	vk_image_create_info.format = supportedFormat;
 	vk_image_create_info.tiling = VK_IMAGE_TILING_OPTIMAL;
-	vk_image_create_info.initialLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
+	vk_image_create_info.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
 	vk_image_create_info.usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
 	vk_image_create_info.samples = VK_SAMPLE_COUNT_1_BIT;
 	vk_image_create_info.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
-
 	VK_CHECK(vkCreateImage(vulkanRenderDevice->GetVkDevice(), &vk_image_create_info, vulkanRenderDevice->GetVkAllocationCallbacks(), &textureImage));
 
 	{
@@ -113,22 +117,8 @@ VulkanTexture::VulkanTexture(VulkanRenderDevice* vulkanRenderDevice, const Textu
 		vkBindImageMemory(vulkanRenderDevice->GetVkDevice(), textureImage, textureImageMemory, 0);
 	}
 
-
-	VkBufferImageCopy region{};
-	region.bufferOffset = 0;
-	region.bufferRowLength = 0;
-	region.bufferImageHeight = 0;
-	region.imageSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-	region.imageSubresource.mipLevel = 0;
-	region.imageSubresource.baseArrayLayer = 0;
-	region.imageSubresource.layerCount = 1;
-	region.imageOffset = { 0, 0, 0 };
-	region.imageExtent = { textureCreateInfo.Extent.Width, textureCreateInfo.Extent.Height, 1 };
-
-	vkCmdCopyBufferToImage(static_cast<VulkanCommandBuffer*>(textureCreateInfo.CommandBuffer)->GetVkCommandBuffer(), staging_buffer, textureImage, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1, &region);
-
 	{
-		VkImageMemoryBarrier barrier = {};
+		VkImageMemoryBarrier barrier{};
 		barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
 		barrier.oldLayout = VK_IMAGE_LAYOUT_UNDEFINED;
 		barrier.newLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
@@ -146,28 +136,28 @@ VulkanTexture::VulkanTexture(VulkanRenderDevice* vulkanRenderDevice, const Textu
 
 		barrier.srcAccessMask = 0;
 		barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
-
 		sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
 		destinationStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
 
-		VkBufferImageCopy region{};
-		region.bufferOffset = 0;
-		region.bufferRowLength = 0;
-		region.bufferImageHeight = 0;
-		region.imageSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-		region.imageSubresource.mipLevel = 0;
-		region.imageSubresource.baseArrayLayer = 0;
-		region.imageSubresource.layerCount = 1;
-		region.imageOffset = { 0, 0, 0 };
-		region.imageExtent = { textureCreateInfo.Extent.Width, textureCreateInfo.Extent.Height, 1 };
-
-		vkCmdCopyBufferToImage(static_cast<VulkanCommandBuffer*>(textureCreateInfo.CommandBuffer)->GetVkCommandBuffer(), staging_buffer, textureImage, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1, &region);
+		vkCmdPipelineBarrier(static_cast<VulkanCommandBuffer*>(textureCreateInfo.CommandBuffer)->GetVkCommandBuffer(), sourceStage, destinationStage, 0, 0, nullptr, 0, nullptr, 1, &barrier);
 	}
+
+	VkBufferImageCopy region{};
+	region.bufferOffset = 0;
+	region.bufferRowLength = 0;
+	region.bufferImageHeight = 0;
+	region.imageSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+	region.imageSubresource.mipLevel = 0;
+	region.imageSubresource.baseArrayLayer = 0;
+	region.imageSubresource.layerCount = 1;
+	region.imageOffset = { 0, 0, 0 };
+	region.imageExtent = { textureCreateInfo.Extent.Width, textureCreateInfo.Extent.Height, 1 };
+	vkCmdCopyBufferToImage(static_cast<VulkanCommandBuffer*>(textureCreateInfo.CommandBuffer)->GetVkCommandBuffer(), staging_buffer, textureImage, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1, &region);
 
 	{
 		auto to_image_layout = ImageLayoutToVkImageLayout(textureCreateInfo.Layout);
 
-		VkImageMemoryBarrier barrier = {};
+		VkImageMemoryBarrier barrier{};
 		barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
 		barrier.oldLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
 		barrier.newLayout = to_image_layout;
@@ -183,15 +173,7 @@ VulkanTexture::VulkanTexture(VulkanRenderDevice* vulkanRenderDevice, const Textu
 		VkPipelineStageFlags sourceStage;
 		VkPipelineStageFlags destinationStage;
 
-		if (VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL == VK_IMAGE_LAYOUT_UNDEFINED && to_image_layout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL)
-		{
-			barrier.srcAccessMask = 0;
-			barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
-
-			sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
-			destinationStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
-		}
-		else if (VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL && to_image_layout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL)
+		if (VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL && to_image_layout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL)
 		{
 			barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
 			barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
@@ -205,12 +187,10 @@ VulkanTexture::VulkanTexture(VulkanRenderDevice* vulkanRenderDevice, const Textu
 		}
 
 		vkCmdPipelineBarrier(static_cast<VulkanCommandBuffer*>(textureCreateInfo.CommandBuffer)->GetVkCommandBuffer(), sourceStage, destinationStage, 0, 0, nullptr, 0, nullptr, 1, &barrier);
-
 	}
 
 	vkDestroyBuffer(vulkanRenderDevice->GetVkDevice(), staging_buffer, vulkanRenderDevice->GetVkAllocationCallbacks());
 	vkFreeMemory(vulkanRenderDevice->GetVkDevice(), staging_buffer_memory, vulkanRenderDevice->GetVkAllocationCallbacks());
-
 
 	VkImageViewCreateInfo vk_image_view_create_info{ VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO };
 	vk_image_view_create_info.image = textureImage;
@@ -221,7 +201,6 @@ VulkanTexture::VulkanTexture(VulkanRenderDevice* vulkanRenderDevice, const Textu
 	vk_image_view_create_info.subresourceRange.levelCount = 1;
 	vk_image_view_create_info.subresourceRange.baseArrayLayer = 0;
 	vk_image_view_create_info.subresourceRange.layerCount = 1;
-
 	VK_CHECK(vkCreateImageView(vulkanRenderDevice->GetVkDevice(), &vk_image_view_create_info, vulkanRenderDevice->GetVkAllocationCallbacks(), &textureImageView));
 
 	VkSamplerCreateInfo vk_sampler_create_info{ VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO };

b02d6d3007350db95778896393600cabb32576cc
Facundo961 <facundovilla961@gmail.com>
2020-03-21 20:40:04 -0300

    Updated InputManager.

--- GS_Engine/src/Game Studio/Application/InputManager.cpp
+++ GS_Engine/src/Game Studio/Application/InputManager.cpp
@@ -1,19 +1,1 @@
 #include "InputManager.h"
-
-#include "RAPI/Window.h"
-
-void InputManager::OnUpdate()
-{
-	auto& MouseState = ActiveWindow->GetMouseState();
-
-	MouseOffset = MouseState.MousePosition - MousePosition;
-	MousePosition = MouseState.MousePosition;
-
-	Mouse = MouseState;
-
-	ScrollWheelDelta = MouseState.MouseWheelMove - ScrollWheelMovement;
-	ScrollWheelMovement = MouseState.MouseWheelMove;
-
-	Keys = ActiveWindow->GetKeyboardKeys();
-	JoystickStates = ActiveWindow->GetJoystickStates();
-}
--- GS_Engine/src/Game Studio/Application/InputManager.h
+++ GS_Engine/src/Game Studio/Application/InputManager.h
@@ -4,7 +4,7 @@
 
 #include "Object.h"
 
-#include "Math\Vector2.h"
+#include "Math/Vector2.h"
 #include "Containers/Array.hpp"
 #include "Input/MouseState.h"
 #include "Input/InputEnums.h"
@@ -13,55 +13,22 @@
 #include "Containers/FVector.hpp"
 #include "Utility/Delegate.h"
 #include <map>
+#include <unordered_map>
+#include "Core/Window.h"
 
 namespace RAPI
 {
-		class Window;
+	class Window;
 }
-
-struct InputAction
-{
-	Id64 Name;
-	FVector<FString> FiringInputEvents;
-};
 
 class InputManager : public Object
 {
-	RAPI::Window* ActiveWindow = nullptr;
-
-	MouseState Mouse;
-
-	Array<bool, MAX_KEYBOARD_KEYS> Keys;
-
-	Array<JoystickState, 4> JoystickStates;
-
-	//std::multimap<Id64::HashType, Pair<Action, FVector<Functor<void()>>>> SingleActions;
-	//std::multimap<Id64::HashType, Pair<Action, FVector<Functor<void()>>>> ContinuousActions;
-
-	//Mouse vars
-
-	//Current mouse position.
-	Vector2 MousePosition;
-
-	//Offset from last frame's mouse position.
-	Vector2 MouseOffset;
-
-	float ScrollWheelMovement = 0.0f;
-	float ScrollWheelDelta = 0.0f;
+	std::unordered_map<Id16, Delegate<void(float)>> axisActions;
+	std::unordered_map<Id16, Delegate<void(bool)>>	buttonActions;
 
 public:
 	InputManager() = default;
 	~InputManager() = default;
-
-	void SetActiveWindow(RAPI::Window* _NewWindow) { ActiveWindow = _NewWindow; }
-
-	[[nodiscard]] MouseState GetMouseState() const { return Mouse; }
-	[[nodiscard]] bool GetKeyState(KeyboardKeys _Key) const { return Keys[SCAST(uint8, _Key)]; }
-	[[nodiscard]] JoystickState GetJoystickState(uint8 _Joystick) const { return JoystickStates[_Joystick]; }
-
-	[[nodiscard]] Vector2 GetMouseOffset() const { return MouseOffset; }
-
-	void OnUpdate() override;
 
 	[[nodiscard]] const char* GetName() const override { return "Input Manager"; }
 };

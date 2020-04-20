22ddb97a77a347d750be3c778e34958a305cdc12
Facundo961 <facundovilla961@gmail.com>
2020-04-20 02:12:28 -0300

    Moved definitions to cpp.

--- Byte Engine/src/Byte Engine/Application/PowerOf2PoolAllocator.h
+++ Byte Engine/src/Byte Engine/Application/PowerOf2PoolAllocator.h
@@ -2,9 +2,10 @@
 
 #include "Byte Engine/Core.h"
 
-#include <GTSL/KeepVector.h>
-#include <GTSL/Math/Math.hpp>
-#include <GTSL/Bitscan.h>
+#include <atomic>
+#include <GTSL/Mutex.h>
+#include <GTSL/Allocator.h>
+#include <GTSL/Vector.hpp>
 
 class PowerOf2PoolAllocator
 {
@@ -26,9 +27,8 @@ class PowerOf2PoolAllocator
 
 			void popFreeSlot(uint32& freeSlot)
 			{
-				freeSlot = freeSlotsIndices()[0];
+				freeSlot = freeSlotsIndices()[freeSlotsCount];
 				--freeSlotsCount;
-				GTSL::Memory::CopyMemory(sizeof(uint32) * freeSlotsCount, freeSlotsIndices() + 1, freeSlotsIndices());
 			}
 
 			void placeFreeSlot(const uint32 freeSlot)
@@ -46,53 +46,15 @@ class PowerOf2PoolAllocator
 			}
 			
 		public:
-			Block(const uint16 slotsCount, const uint32 slotsSize, uint64& allocatedSize, GTSL::AllocatorReference* allocatorReference)
-			{				
-				const uint64 free_indeces_stack_space = slotsCount * sizeof(uint32);
-				const uint64 block_data_space = slotsSize * slotsCount;
-				
-				allocatorReference->Allocate(free_indeces_stack_space + block_data_space, alignof(uint32), reinterpret_cast<void**>(&data), &allocatedSize);
-				
-				for(uint32 i = 0; i < slotsCount; ++i)
-				{
-					freeSlotsIndices()[i] = i;
-				}
-			}
+			Block(uint16 slotsCount, uint32 slotsSize, uint64& allocatedSize, GTSL::AllocatorReference* allocatorReference);
 
-			void FreeBlock(const uint32 slotsSize, const uint16 slotsCount, uint64& freedSpace, GTSL::AllocatorReference* allocatorReference)
-			{
-				freedSpace = slotsSize * slotsCount + slotsCount * sizeof(uint32);
-				allocatorReference->Deallocate(freedSpace, alignof(uint32), data);
-				data = nullptr;
-			}
-			
-			bool DoesAllocationBelongToBlock(void* p, const uint16 slotsCount, const uint32 slotsSize) const { return p > blockData(slotsCount) && p < blockDataEnd(slotsCount, slotsSize); }
+			void FreeBlock(uint16 slotsCount, uint32 slotsSize, uint64& freedSpace, GTSL::AllocatorReference* allocatorReference);
 
-			void AllocateInBlock(const uint64 alignment, void** data, uint64& allocatedSize, const uint16 slotsCount, const uint32 slotsSize)
-			{
-				uint32 free_slot{ 0 };
-				mutex.Lock();
-				popFreeSlot(free_slot);
-				mutex.Unlock();
-				*data = GTSL::Memory::AlignedPointer(alignment, blockData(slotsCount) + free_slot * slotsSize);
-				allocatedSize = slotsSize - ((blockData(slotsCount) + (free_slot + 1) * slotsSize) - reinterpret_cast<byte*>(*data));
-			}
+			bool DoesAllocationBelongToBlock(void* p, uint16 slotsCount, const uint32 slotsSize) const { return p > blockData(slotsCount) && p < blockDataEnd(slotsCount, slotsSize); }
 
-			bool TryAllocateInBlock(const uint64 alignment, void** data, uint64& allocatedSize, const uint16 slotsCount, const uint32 slotsSize)
-			{
-				uint32 free_slot{ 0 };
-				mutex.Lock();
-				if (freeSlot())
-				{
-					popFreeSlot(free_slot);
-					mutex.Unlock();
-					*data = GTSL::Memory::AlignedPointer(alignment, blockData(slotsCount) + free_slot * slotsSize);
-					allocatedSize = slotsSize - ((blockData(slotsCount) + (free_slot + 1) * slotsSize) - reinterpret_cast<byte*>(*data));
-					return true;
-				}
-				mutex.Unlock();
-				return false;
-			}
+			void AllocateInBlock(uint64 alignment, void** data, uint64& allocatedSize, uint16 slotsCount, uint32 slotsSize);
+
+			bool TryAllocateInBlock(uint64 alignment, void** data, uint64& allocatedSize, uint16 slotsCount, uint32 slotsSize);
 
 			void DeallocateInBlock(uint64 alignment, void* data, const uint16 slotsCount, const uint32 slotsSize)
 			{
@@ -107,96 +69,29 @@ class PowerOf2PoolAllocator
 		std::atomic<uint32> index{ 0 };
 		const uint16 slotsCount{ 0 };
 		const uint32 slotsSize{ 0 };
-	public:
-		Pool(uint16 slotsCount, uint32 slotsSize, const uint8 blockCount, uint64& allocatedSize, GTSL::AllocatorReference* allocatorReference) : blocks(blockCount, allocatorReference)
-		{	
-			for(uint8 i = 0; i < blockCount; ++i)
-			{
-				blocks.EmplaceBack(slotsCount, slotsSize, allocatedSize, allocatorReference);
-			}
-		}
 		
-		void Allocate(const uint64 size, const uint64 alignment, void** data, uint64* allocatedSize, GTSL::AllocatorReference* allocatorReference)
-		{
-			BE_ASSERT(size > slotsSize, "Allocation size greater than pool's slot size")
-			BE_ASSERT(GTSL::Math::AlignedNumber(alignment, size) > slotsSize, "Aligned allocation size greater than pool's slot size")
-			
-			blocksMutex.ReadLock();
-			const auto i{ index % blocks.GetLength() };
-			blocksMutex.ReadUnlock();
-			++index;
+	public:
+		Pool(uint16 slotsCount, uint32 slotsSize, uint8 blockCount, uint64& allocatedSize, GTSL::AllocatorReference* allocatorReference);
 
-			blocksMutex.ReadLock();
-			for(uint32 j = 0; j < blocks.GetLength(); ++j)
-			{
-				if (blocks[(i + j) % blocks.GetLength()].TryAllocateInBlock(alignment, data, *allocatedSize,slotsCount, slotsSize)) { blocksMutex.ReadUnlock(); return; }
-			}
-			blocksMutex.ReadUnlock();
-			
-			blocksMutex.WriteLock();
-			blocks[blocks.EmplaceBack(slotsSize, slotsCount, allocatorReference)].AllocateInBlock(alignment, data, *allocatedSize, slotsCount, slotsSize);
-			blocksMutex.WriteUnlock();
-		}
+		void Free(uint64& freedBytes, GTSL::AllocatorReference* allocatorReference);
 
-		void Deallocate(uint64 size, const uint64 alignment, void* memory, GTSL::AllocatorReference* allocatorReference)
-		{
-			blocksMutex.ReadLock();
-			for(auto& e : blocks)
-			{
-				if(e.DoesAllocationBelongToBlock(memory, slotsCount, slotsSize))
-				{
-					e.DeallocateInBlock(alignment, memory, slotsCount, slotsSize);
-					blocksMutex.ReadUnlock();
-					return;
-				}
-			}
-			blocksMutex.ReadUnlock();
+		void Allocate(uint64 size, uint64 alignment, void** data, uint64* allocatedSize, uint64& allocatorAllocatedBytes, GTSL::AllocatorReference* allocatorReference);
 
-			BE_ASSERT(true, "Allocation couldn't be freed from this pool, pointer does not belong to any allocation in this pool!")
-		}
+		void Deallocate(uint64 size, uint64 alignment, void* memory, GTSL::AllocatorReference* allocatorReference);
 	};
 	
 	GTSL::Vector<Pool> pools;
 public:
-	PowerOf2PoolAllocator(GTSL::AllocatorReference* allocatorReference) : allocatorReference(allocatorReference)
+	PowerOf2PoolAllocator(GTSL::AllocatorReference* allocatorReference);
+
+	~PowerOf2PoolAllocator()
 	{
-		const auto max_power_of_two_allocatable = 10;
-
-		uint64 allocated_size{ 0 }; //debug
-		
-		for(uint32 i = max_power_of_two_allocatable; i > 0; --i)
-		{
-			pools.EmplaceBack(i * max_power_of_two_allocatable, 1 << i, i, allocated_size, allocatorReference);
-		}
-	}
-	
-	void Allocate(const uint64 size, const uint64 alignment, void** memory, uint64* allocatedSize, const char* name)
-	{
-		BE_ASSERT((alignment & (alignment - 1)) != 0, "Alignment is not power of two!")
-
-		uint64 allocation_min_size{ 0 };
-		GTSL::NextPowerOfTwo(size, allocation_min_size);
-		
-		BE_ASSERT((allocation_min_size& (allocation_min_size - 1)) != 0, "allocation_min_size is not power of two!")
-		
-		uint8 set_bit{ 0 };
-		GTSL::BitScanForward(allocation_min_size, set_bit);
-
-		pools[set_bit].Allocate(size, alignment, memory, allocatedSize, allocatorReference);
+		Free();
 	}
 
-	void Deallocate(const uint64 size, const uint64 alignment, void* memory, const char* name)
-	{
-		BE_ASSERT((alignment & (alignment - 1)) != 0, "Alignment is not power of two!")
+	void Free();
 
-		const uint64 allocation_min_size{ GTSL::Math::AlignedNumber(size, alignment) };
-		BE_ASSERT((allocation_min_size& (allocation_min_size - 1)) != 0, "allocation_min_size is not power of two!")
-		
-		uint8 set_bit{ 0 };
-		GTSL::BitScanForward(allocation_min_size, set_bit);
+	void Allocate(uint64 size, uint64 alignment, void** memory, uint64* allocatedSize, const char* name);
 
-		pools[set_bit].Deallocate(size, alignment, memory, allocatorReference);
-	}
-
-	
+	void Deallocate(uint64 size, uint64 alignment, void* memory, const char* name);
 };

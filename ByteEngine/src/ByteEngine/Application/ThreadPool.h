75a4770ceefd5de6ee09a90cf727c6772bc326f0
Facundo Villa <facundovilla961@gmail.com>
2020-07-08 15:43:48 -0300

    Updated EnqueTask implementation.

--- ByteEngine/src/ByteEngine/Application/ThreadPool.h
+++ ByteEngine/src/ByteEngine/Application/ThreadPool.h
@@ -6,9 +6,9 @@
 #include <GTSL/Atomic.hpp>
 #include <GTSL/Delegate.hpp>
 #include <GTSL/BlockingQueue.h>
-#include <GTSL/ConditionVariable.h>
 #include <thread>
 #include <GTSL/Thread.h>
+#include <GTSL/Tuple.h>
 
 //https://github.com/mvorbrodt/blog
 
@@ -18,18 +18,18 @@ public:
 	explicit ThreadPool() : queues(threadCount)
 	{
 		//lambda
-		auto workers_loop = [this](const uint8 i)
+		auto workers_loop = [](ThreadPool* pool, const uint8 i)
 		{
 			while (true)
 			{
-				GTSL::Delegate<void()> work;
+				GTSL::Delegate<void(void*)> work;
 				for (auto n = 0; n < threadCount * K; ++n)
 				{
-					if (queues[(i + n) % threadCount].TryPop(work)) { break; }
+					if (pool->queues[(i + n) % threadCount].TryPop(work)) { break; }
 				}
 
-				if (!work && !queues[i].Pop(work)) { break; }
-				work();
+				if (!work && !pool->queues[i].Pop(work)) { break; }
+				work(pool->taskDatas[i]);
 			}
 		};
 
@@ -37,8 +37,8 @@ public:
 		{
 			//Constructing threads with function and I parameter
 			//threads.EmplaceBack(workers_loop, i);
-			threads.EmplaceBack(GTSL::Delegate<void(const uint8)>::Create(workers_loop), i);
-			//threads.SetThreadPriority(HIGH);
+			threads.EmplaceBack(GTSL::Delegate<void(ThreadPool*, uint8)>::Create(workers_loop), this, i);
+			threads[i].SetPriority(GTSL::Thread::Priority::HIGH);
 		}
 	}
 
@@ -49,34 +49,50 @@ public:
 	}
 
 	template<typename F, typename... ARGS>
-	void EnqueueTask(const GTSL::Delegate<F>& delegate, GTSL::Semaphore* conditionVariable, ARGS&&... args)
-	{	
-		auto work = [delegate, conditionVariable, ... args = GTSL::MakeForwardReference<ARGS>(args)]()
+	void EnqueueTask(const GTSL::Delegate<F>& delegate, GTSL::Semaphore* semaphore, ARGS&&... args)
+	{
+		auto task = new TaskInfo<F, ARGS...>(delegate, semaphore, GTSL::MakeForwardReference<ARGS>(args)...);
+		
+		auto work = [](void* voidTask)
 		{
-			delegate(GTSL::MakeForwardReference<ARGS>(args)...);
-			conditionVariable->Post();
-		};
+			TaskInfo<F, ARGS...>* task = static_cast<TaskInfo<F, ARGS...>*>(voidTask);
+			GTSL::Thread::Call<F, ARGS...>(task->Delegate, task->Arguments);
+			task->Semaphore->Post();
 
+			delete task;
+		};
+		
 		const auto current_index = ++index;
 
 		for (auto n = 0; n < threadCount * K; ++n)
 		{
 			//Try to Push work into queues, if success return else when Done looping place into some queue.
 
-			if (queues[(current_index + n) % threadCount].TryPush(GTSL::Delegate<void()>::Create(work))) { return; }
+			if (queues[(current_index + n) % threadCount].TryPush(GTSL::Delegate<void(void*)>::Create(work))) { return; }
 		}
 
-		queues[current_index % threadCount].Push(GTSL::Delegate<void()>::Create(work));
+		queues[current_index % threadCount].Push(GTSL::Delegate<void(void*)>::Create(work));
 	}
 
 private:
 	inline const static uint8 threadCount{ static_cast<uint8>(std::thread::hardware_concurrency() - 1) };
 	GTSL::Atomic<uint32> index{ 0 };
 	
-	GTSL::Array<GTSL::BlockingQueue<GTSL::Delegate<void()>>, 64> queues;
+	GTSL::Array<GTSL::BlockingQueue<GTSL::Delegate<void(void*)>>, 64> queues;
 	GTSL::Array<GTSL::Thread, 64> threads;
+	GTSL::Array<void*, 128> taskDatas;
 
-
+	template<typename T, typename... ARGS>
+	struct TaskInfo
+	{
+		TaskInfo(const GTSL::Delegate<T>& delegate, GTSL::Semaphore* semaphore, ARGS&&... args) : Delegate(delegate), Semaphore(semaphore), Arguments(GTSL::MakeForwardReference<ARGS>(args)...)
+		{
+		}
+		
+		GTSL::Delegate<T> Delegate;
+		GTSL::Tuple<ARGS...> Arguments;
+		GTSL::Semaphore* Semaphore = nullptr;
+	};
 	/**
 	 * \brief Number of times to loop around the queues to find one that is free.
 	 */

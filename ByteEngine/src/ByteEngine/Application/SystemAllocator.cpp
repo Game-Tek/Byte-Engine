4d3c674e70f04b1347eaa84c7edf465457141407
Facundo Villa <facundovilla961@gmail.com>
2020-07-21 16:57:25 -0300

    Made SystemAllocator return aligned allocations.

--- ByteEngine/src/ByteEngine/Application/SystemAllocator.cpp
+++ ByteEngine/src/ByteEngine/Application/SystemAllocator.cpp
@@ -3,35 +3,33 @@
 void SystemAllocator::Allocate(const uint64 size, const uint64 alignment, void** data)
 {
 	const uint64 allocated_size{ GTSL::Math::PowerOf2RoundUp(size, static_cast<uint32>(alignment)) };
-	void* raw_data_alloc{nullptr};
 
 	allocatorMutex.Lock();
-	GTSL::Allocate(size, &raw_data_alloc);
+	GTSL::Allocate(allocated_size, data);
 	allocatorMutex.Unlock();
 
-	//*data = GTSL::Memory::AlignedPointer(alignment, raw_data_alloc);
-	//::new(static_cast<byte*>(*data)) (void*)(raw_data_alloc);
-
-	*data = raw_data_alloc;
+	*data = GTSL::AlignPointer(alignment, data);
 
 	BE_DEBUG_ONLY(GTSL::Lock<GTSL::Mutex> lock(debugDataMutex))
-	BE_DEBUG_ONLY(allocatedBytes += size)
-	BE_DEBUG_ONLY(totalAllocatedBytes += size)
+	BE_DEBUG_ONLY(allocatedBytes += allocated_size)
+	BE_DEBUG_ONLY(totalAllocatedBytes += allocated_size)
 	BE_DEBUG_ONLY(++allocationCount)
 	BE_DEBUG_ONLY(++totalAllocationCount)
 }
 
 void SystemAllocator::Deallocate(const uint64 size, const uint64 alignment, void* data)
 {
-	const uint64 deallocated_size{GTSL::Math::PowerOf2RoundUp(size, static_cast<uint32>(alignment))};
+	const uint64 allocation_size{GTSL::Math::PowerOf2RoundUp(size, static_cast<uint32>(alignment))};
 
+	byte* dealigned_pointer = static_cast<byte*>(data) - (allocation_size - size);
+	
 	allocatorMutex.Lock();
-	GTSL::Deallocate(size, data);
+	GTSL::Deallocate(allocation_size, dealigned_pointer);
 	allocatorMutex.Unlock();
 
 	BE_DEBUG_ONLY(GTSL::Lock<GTSL::Mutex> lock(debugDataMutex))
-	BE_DEBUG_ONLY(deallocatedBytes += size)
-	BE_DEBUG_ONLY(totalDeallocatedBytes += size)
+	BE_DEBUG_ONLY(deallocatedBytes += allocation_size)
+	BE_DEBUG_ONLY(totalDeallocatedBytes += allocation_size)
 	BE_DEBUG_ONLY(++deallocationCount)
 	BE_DEBUG_ONLY(++totalDeallocationCount)
 }

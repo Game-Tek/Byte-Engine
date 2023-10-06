731b3822d0a82aa3d172b1ffa4c7d674374960f4
facundo-villa <facundovilla961@gmail.com>
2023-10-06 17:46:33 -0300

    Added ortho matrix.

--- src/math.rs
+++ src/math.rs
@@ -22,3 +22,12 @@ pub fn projection_matrix(fov: f32, aspect_ratio: f32, near_plane: f32, far_plane
 		maths_rs::Vec4f::from((0f32,					0f32, 		1f32, 													0f32)),
 	))
 }
+
+pub fn orthographic_matrix(width: f32, height: f32, near_plane: f32, far_plane: f32) -> maths_rs::Mat4f {
+	maths_rs::Mat4f::from((
+		maths_rs::Vec4f::from((2f32 / width, 	0f32, 			0f32, 													0f32)),
+		maths_rs::Vec4f::from((0f32, 			2f32 / height,	0f32, 													0f32)),
+		maths_rs::Vec4f::from((0f32, 			0f32, 			2f32 / (far_plane - near_plane), 						0f32)),
+		maths_rs::Vec4f::from((0f32,			0f32, 			-((far_plane + near_plane) / (far_plane - near_plane)),	1f32)),
+	))
+}

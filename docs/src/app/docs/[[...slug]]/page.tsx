d075d2fe56bccf20ed038d1e8f615e40bf4749eb
Facundo Villa <facundovilla961@gmail.com>
2025-05-29 00:35:49 -0300

    remove OG generation

--- docs/src/app/docs/[[...slug]]/page.tsx
+++ docs/src/app/docs/[[...slug]]/page.tsx
@@ -47,17 +47,8 @@ export async function generateMetadata(props: {
 	const page = source.getPage(slug);
 	if (!page) notFound();
 
-	const image = ['/docs-og', ...slug, 'image.png'].join('/');
-
 	return {
 		title: page.data.title,
 		description: page.data.description,
-		openGraph: {
-			images: image,
-		},
-		twitter: {
-			card: 'summary_large_image',
-			images: image,
-		},
 	};
 }

e5f7d87c793c89a5049917a1ec9de4193c8a6b72
Facundo Villa <facundovilla961@gmail.com>
2025-05-30 00:51:39 -0300

    remove horrible nextjs

---
+++ docs/app.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from '@tanstack/react-start/config';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+	server: {
+		hooks: {
+			'prerender:routes': async (routes) => {
+				const { source } = await import('./lib/source');
+				const pages = source.getPages();
+
+				for (const page of pages) {
+					routes.add(page.url);
+				}
+			},
+		},
+		prerender: {
+			routes: ['/'],
+			crawlLinks: true,
+		},
+	},
+	vite: {
+		build: {
+			rollupOptions: {
+				external: ['shiki'],
+				onwarn(warning, warn) {
+					if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+						return;
+					}
+					warn(warning);
+				},
+			},
+		},
+		plugins: [
+			tsConfigPaths({
+				projects: ['./tsconfig.json'],
+			}),
+			tailwindcss(),
+		],
+	},
+});
---
+++ docs/lib/source.ts
@@ -0,0 +1,86 @@
+import {
+	loader,
+	type MetaData,
+	type PageData,
+	type Source,
+	type VirtualFile,
+} from 'fumadocs-core/source';
+import matter from 'gray-matter';
+import * as path from 'node:path';
+import { globSync } from 'tinyglobby';
+import * as fs from 'node:fs';
+import { icons } from 'lucide-react';
+import { createElement } from 'react';
+
+let files: [string, string][];
+
+if (typeof import.meta.glob === 'function') {
+	files = Object.entries(
+		import.meta.glob<true, 'raw'>('/content/docs/**/*', {
+			eager: true,
+			query: '?raw',
+			import: 'default',
+		}),
+	);
+} else {
+	files = globSync('content/docs/**/*').map((file) => {
+		return [file, fs.readFileSync(file).toString()];
+	});
+}
+
+const virtualFiles: VirtualFile[] = files.flatMap(([file, content]) => {
+	const ext = path.extname(file);
+	const virtualPath = path.relative(
+		'content/docs',
+		path.join(process.cwd(), file),
+	);
+
+	if (ext === '.mdx' || ext === '.md') {
+		const parsed = matter(content);
+
+		return {
+			type: 'page',
+			path: virtualPath,
+			data: {
+				...parsed.data,
+				content: parsed.content,
+			},
+		};
+	}
+
+	if (ext === '.json') {
+		return {
+			type: 'meta',
+			path: virtualPath,
+			data: JSON.parse(content),
+		};
+	}
+
+	return [];
+});
+
+export const source = loader({
+	source: {
+		files: virtualFiles,
+	} as Source<{
+		pageData: PageData & {
+		content: string;
+		};
+		metaData: MetaData;
+	}>,
+	baseUrl: '/docs',
+	// icon(icon) {
+	// 	if (!icon) {
+	// 		return;
+	// 	}
+
+	// 	const IconComponent = icons[icon as keyof typeof icons];
+
+	// 	if (!IconComponent) {
+	// 		console.warn(`Icon component not found for icon: ${icon}`);
+	// 		return;
+	// 	}
+
+	// 	return createElement(IconComponent, { className: 'inline-block h-4 w-4' });
+	// },
+});

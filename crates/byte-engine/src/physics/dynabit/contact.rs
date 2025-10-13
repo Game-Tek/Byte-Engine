09ec5061543d4debc986a1835ba496dab0729bb7
Facundo Villa <facundovilla961@gmail.com>
2025-10-12 23:53:06 -0300

    forgotten files

---
+++ crates/byte-engine/src/physics/dynabit/contact.rs
@@ -0,0 +1,14 @@
+use math::Vector3;
+
+pub struct Contact {
+	pub(crate) a: Side,
+	pub(crate) b: Side,
+	pub(crate) normal: Vector3,
+}
+
+pub struct Side {
+	/// The object handle for this side of the contact.
+	pub(crate) object: usize,
+	/// The point in the world where the contact ocurred.
+	pub(crate) point: Vector3,
+}
---
+++ crates/math/src/ray.rs
@@ -0,0 +1,16 @@
+
+use crate::Vector3;
+
+pub struct Ray {
+	pub(crate) origin: Vector3,
+	pub(crate) direction: Vector3,
+}
+
+impl Ray {
+	pub fn new(origin: Vector3, direction: Vector3) -> Self {
+		Self {
+			origin,
+			direction,
+		}
+	}
+}

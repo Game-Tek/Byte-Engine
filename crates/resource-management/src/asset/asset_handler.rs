ce939256ccd096e682a687ca0ce45eb612f39138
Facundo Villa <facundovilla961@gmail.com>
2026-01-31 16:19:58 -0300

    make asset generation async

--- crates/resource-management/src/asset/asset_handler.rs
+++ crates/resource-management/src/asset/asset_handler.rs
@@ -14,7 +14,13 @@ pub enum LoadErrors {
 pub trait AssetHandler: Send + Sync {
 	fn can_handle(&self, r#type: &str) -> bool;
 
-	fn load<'a>(&'a self, asset_manager: &'a AssetManager, storage_backend: &'a dyn resource::StorageBackend, asset_storage_backend: &'a dyn asset::StorageBackend, url: ResourceId<'a>,) -> Result<Box<dyn Asset>, LoadErrors>;
+	fn load<'a>(
+		&'a self,
+		asset_manager: &'a AssetManager,
+		storage_backend: &'a dyn resource::StorageBackend,
+		asset_storage_backend: &'a dyn asset::StorageBackend,
+		url: ResourceId<'a>,
+	) -> BoxedFutureFuture<'a, Result<Box<dyn Asset>, LoadErrors>>;
 
 	fn produce<'a>(&'a self, _: ResourceId<'a>, _: &'a dyn crate::Description, _: Box<[u8]>) -> Result<(ProcessedAsset, Box<[u8]>), String> {
 		unimplemented!()

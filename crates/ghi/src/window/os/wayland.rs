0ad6326c679ee93855cac695fb0c24298cce6a15
Facundo Villa <facundovilla961@gmail.com>
2026-08-15 00:51:59 -0300

    hmm

--- crates/ghi/src/window/os/wayland.rs
+++ crates/ghi/src/window/os/wayland.rs
@@ -171,7 +171,7 @@ mod input;
 mod key_translation;
 mod lifecycle;
 
-use key_translation::{KeyboardState, keysym_to_key};
+use key_translation::{keysym_to_key, KeyboardState};
 
 #[cfg(test)]
 mod tests {
